4c3162b189da7ec51efe2341211a38b915092a3e
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-12-01 08:29:13 +0200

    04 connectiong to mongo part one

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+// mongodb+srv://Viktor:<password>@cluster0.qsqj6.mongodb.net/?retryWrites=true&w=majority
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+//create out express app
+const app = express()
+
+//Handle CORS + middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE"); // If using .fetch and not axios
+  res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+//database stuff
+const uri = "mongodb+srv://Viktor:<password>@cluster0.qsqj6.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB connected")
+})
+.catch(err => console.log(err))
