df0a0f61da09f550573497b2133f796917b77310
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-12-01 09:11:25 +0200

    06 MEVN - 5 - Express Model Schema setup

--- backend/models/Todos.js
+++ backend/models/Todos.js
@@ -3,5 +3,5 @@ const TodosSchema = new mongoose.Schema({
   todo: String,
   author: String
 })
-
+//I've already added models in previous lesson
 module.exports = mongoose.model('todo', TodosSchema)
